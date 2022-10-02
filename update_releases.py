dc81b69c5271ccd28320ca3f7f7504759f0d8d4c
Farzam Mohammadi <farzamfts@hotmail.com>
2022-10-01 22:50:19 -0400

    Implement critical release update handling

--- update_releases.py
+++ update_releases.py
@@ -7,7 +7,7 @@ from azure.devops.v5_1.release.models import ReleaseEnvironmentUpdateMetadata
 from dotenv import load_dotenv
 from msrest.authentication import BasicAuthentication
 
-from utils import get_data_from_deployment_plan_file, release_updated_successfuly
+from utils import get_data_from_deployment_plan_file, handle_failed_update, release_updated_successfuly
 from constants import ENVIRONMENT_STATUSES
 
 logging.basicConfig(filename='deployment_error.log', encoding='utf-8', level=logging.INFO)
@@ -55,22 +55,27 @@ try:
         for environment in release_to_update_data.environments:
             if (str(environment.name).lower() == release_stage_name.lower()):
                 matching_release_environment = environment
+        
+        if matching_release_environment is not None:
+            if matching_release_environment.status not in ENVIRONMENT_STATUSES.IN_PROGRESS:
+                # Update Release
+                environment_update_data = ReleaseEnvironmentUpdateMetadata(comment='', status=2)
+                update = release_client_v6.update_release_environment(environment_update_data=environment_update_data, project=deployment_to_update.release_project_name, release_id=release_to_update_data.id, environment_id=matching_release_environment.id)
+            else: logging.info(f'Message:Release is already updating - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
-        if matching_release_environment.status not in ENVIRONMENT_STATUSES.IN_PROGRESS:
-            # Update Release
-            environment_update_data = ReleaseEnvironmentUpdateMetadata(comment='', status=2)
-            update = release_client_v6.update_release_environment(environment_update_data=environment_update_data, project=deployment_to_update.release_project_name, release_id=release_to_update_data.id, environment_id=matching_release_environment.id)
-        else: logging.info(f'Message:Release is already updating - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+            # Check the status of release update
+            logging.info(f'Message:Monitoring Update Status - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+            updated_successfully = release_updated_successfuly(release_client, deployment_to_update.release_project_name, release_stage_name, matching_release_to_update.id)
 
-        # Check the status of release update
-        logging.info(f'Message:Monitoring Update Status - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-        updated_successfully = release_updated_successfuly(release_client, deployment_to_update.release_project_name, release_stage_name, matching_release_to_update.id)
-
-        if not updated_successfully:
-            logging.error(f'Message:Release Update Unsuccessful - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-        else:
-            logging.info(f'Message:Release Update Successful - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-
+            if not updated_successfully:
+                handle_failed_update(deployment_to_update.critical, deployment_to_update)
+            else:
+                logging.info(f'Message:Release Update Successful - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} Destination Environment:{release_stage_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        else: 
+            failure_reason = f'Destination Environment "{release_stage_name}" not found'
+            handle_failed_update(deployment_to_update.critical, deployment_to_update, failure_reason)
+            continue
+            
 except Exception as e:
     logging.error(f'There was an error updating the deployments. Please check their status and continue manually.\nException:{e}')
 
