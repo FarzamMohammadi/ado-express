3c4c833e4453bc4f3ac045dabff937bead837a2b
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-10-01 22:52:37 -0400

    Implement critical release update handling (#4)
    Add critical header to excel file
    Include critical in release-to-update object
    Add method for handling failed updates
    Implement critical update handling in update failed method

--- models.py
+++ models.py
@@ -1,17 +1,33 @@
-class Deployment_Details():
-     """
-    :param release_project_name:
-    :type release_project_name: str
-    :param release_name:
-    :type release_name: str
-    :param release_number:
-    :type release_number: int
-    :param release_rollback:
-    :type release_rollback: int
-    """
+import datetime
+import logging
 
-     def __init__(self, release_project_name=None, release_name=None, release_number=None, release_rollback=None):
-        self.release_project_name = release_project_name
-        self.release_name = release_name
-        self.release_number = release_number
-        self.release_rollback = release_rollback
+from distutils.util import strtobool
+
+class Deployment_Details():
+   """
+   :param release_project_name:
+   :type release_project_name: str
+   :param release_name:
+   :type release_name: str
+   :param release_number:
+   :type release_number: int
+   :param release_rollback:
+   :type release_rollback: int
+   :type critical: bool
+   :param critical:
+   """
+
+   def __init__(self, release_project_name=None, release_name=None, release_number=None, release_rollback=None, critical=False):
+      self.release_project_name = release_project_name
+      self.release_name = release_name
+      self.release_number = release_number
+      self.release_rollback = release_rollback
+
+      if critical is None: 
+         self.critical = False
+      else :
+         try:
+            self.critical = bool(strtobool(critical))
+         except:
+            logging.error(f'Message:Unable to read value of "Critical" - At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
--- utils.py
+++ utils.py
@@ -1,17 +1,30 @@
-import pandas as pd
+from asyncio import exceptions
+import datetime
+import logging
 import time
+import numpy as np
+import pandas as pd
 
 from models import Deployment_Details
 from constants import ENVIRONMENT_STATUSES
 
 def get_data_from_deployment_plan_file():
-    release_dataframe = pd.read_excel('./files/deployment-plan.xlsx').apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    release_dataframe = pd.read_excel('./files/deployment-plan.xlsx').replace(np.nan, None, regex=True).apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     deployment_details = []
     
     for index, row in release_dataframe.iterrows():
-        deployment_details.append(Deployment_Details(row['Project Name'].strip(), row['Release Name'].strip().lower(), row['Release Number'], row['Rollback Number']))
+        deployment_details.append(Deployment_Details(row['Project Name'].strip(), row['Release Name'].strip().lower(), row['Release Number'], row['Rollback Number'], row['Critical']))
 
     return deployment_details
+
+def handle_failed_update(critical, deployment_to_update,  reason=None):
+    if reason is not None:
+        logging.error(f'Message:Release Update Unsuccessful - Reason: {reason} - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    else:
+        logging.error(f'Message:Release Update Unsuccessful - Reason: Please check logs - Project:{deployment_to_update.release_project_name} Release:{deployment_to_update.release_name} At:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+    if deployment_to_update.critical is True:
+        raise Exception('A critical release update failed. Stopping process...')
 
 def release_updated_successfuly(release_client, project_name, stage_name, release_id):
     updated_successfully = False
