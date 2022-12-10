5abfc30c89cb48dad60d46cdac06755d95522529
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-12-10 00:00:51 -0500

    Add files

--- ado_express/packages/common/constants.py
+++ ado_express/packages/common/constants.py
@@ -3,8 +3,8 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Constants:
-    DEPLOYMENT_PLAN_FILE_PATH = os.path.join(ROOT_DIR,'../../../files/deployment/deployment-plan.xlsx')
+    DEPLOYMENT_PLAN_FILE_PATH = os.path.join(ROOT_DIR,'../../files/deployment/deployment-plan.xlsx')
     DEPLOYMENT_PLAN_HEADERS = ['Project Name', 'Release Name', 'Release Number', 'Rollback Number', 'Crucial']
-    LOG_FILE_PATH = os.path.join(ROOT_DIR,'../../../files/logs/deployment.log')
-    SEARCH_RESULTS_FILE_PATH = os.path.join(ROOT_DIR,'../../../files/search-results/search-results.txt')
-    SEARCH_RESULTS_DEPLOYMENT_PLAN_FILE_PATH = os.path.join(ROOT_DIR,'../../../files/search-results/deployment-plan.xlsx')
+    LOG_FILE_PATH = os.path.join(ROOT_DIR,'../../files/logs/deployment.log')
+    SEARCH_RESULTS_FILE_PATH = os.path.join(ROOT_DIR,'../../files/search-results/search-results.log')
+    SEARCH_RESULTS_DEPLOYMENT_PLAN_FILE_PATH = os.path.join(ROOT_DIR,'../../files/search-results/deployment-plan.xlsx')
