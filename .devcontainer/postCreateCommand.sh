b9152f888655a57c533103a429ed4922d252f1e3
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-12-10 14:20:53 -0500

    Add files (#22)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,7 @@ python --version
 pip3 install --upgrade pip
 pip3 install --user -r /workspaces/ado-express/requirements.txt
 if [ -f files/logs/deployment.log ]; then 
-    cat files/logs/deployment.log >> files/logs/deployment_stale.log
+    cat files/logs/deployment.log >> files/logs/deployment-stale.log
 fi
 echo > files/logs/deployment.log
 tail -f files/logs/deployment.log & python ado_express/main.py
