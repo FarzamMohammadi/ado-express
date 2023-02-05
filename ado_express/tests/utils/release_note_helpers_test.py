d8e976da8a3533746500de151c0d69353c00d5ab
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-02-05 12:39:05 -0500

    Revert "testing pr rules (#52)"
    This reverts commit cd1ee14ec47ee5943dfef15e0d5b8ed26f3e78e5.

--- ado_express/tests/utils/release_note_helpers_test.py
+++ ado_express/tests/utils/release_note_helpers_test.py
@@ -4,4 +4,4 @@ from packages.utils.release_note_helpers import needs_deployment
 def test_needs_deployment():
     target = 155
     rollback = 160
-    assert needs_deployment(target, rollback) == True
+    assert needs_deployment(target, rollback) == False
