45ca494d9db552862db3718561297946802d763f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-02-06 19:07:16 -0500

    Add all update_release unit tests

--- ado_express/tests/utils/release_manager/update_release_test.py
+++ ado_express/tests/utils/release_manager/update_release_test.py
@@ -10,6 +10,65 @@ class Empty:
     pass
 
 class UpdateReleaseTests(unittest.TestCase):
+    @patch('packages.utils.release_manager.update_release.UpdateRelease.release_client.get_release', return_value=None)
+    @patch('packages.utils.release_manager.update_release.UpdateRelease', return_value=None)
+    def test_get_release_update_result(self, mock_update_release, mock_release_client):
+        # Arrange
+        fake = Faker()
+
+        deployment_detail = Empty()
+        deployment_detail.release_project_name = fake.name()
+
+        release_to_update = Empty()
+        release_to_update.id = fake.name()
+
+        mock_update_release.environment_variables = Empty()
+        mock_update_release.environment_variables.RELEASE_TARGET_ENV = fake.name()
+
+        updated_release_environment = Empty()
+        updated_release_environment.name = mock_update_release.environment_variables.RELEASE_TARGET_ENV
+        updated_release_environment.status = ReleaseEnvironmentStatuses.Succeeded.SUCCEEDED
+
+        updated_deployment_details = Empty()
+        updated_deployment_details.environments = [updated_release_environment]
+
+        mock_release_client.return_value = updated_deployment_details
+
+        # Act
+        actual_result = UpdateRelease.get_release_update_result(mock_update_release, deployment_detail, release_to_update)
+        
+        # Assert
+        self.assertTrue(actual_result)
+
+    @patch('packages.utils.release_manager.update_release.UpdateRelease', return_value=None)
+    def test_handle_failed_update_crucial(self, mock_update_release):
+        # Arrange
+        fake = Faker()
+        failure_reason = fake.name()
+        deployment_detail = DeploymentDetails(fake.name(), fake.name(), 123, 321, True)
+        exception_message = 'A curcial release update failed. Roll back was attempted. Now, stopping the process.'
+        
+        # Act
+        with self.assertRaises(Exception) as context:
+            UpdateRelease.handle_failed_update(mock_update_release, deployment_detail, False, failure_reason)     
+        
+        # Assert
+        self.assertTrue(exception_message in str(context.exception))
+
+    @patch('packages.utils.release_manager.update_release.UpdateRelease', return_value=None)
+    def test_handle_failed_update_noncrucial(self, mock_update_release):
+        # Arrange
+        fake = Faker()
+        deployment_detail = DeploymentDetails(fake.name(), fake.name(), 123, 321, False)
+
+        noncrucial_message = [f'ERROR:root:Release Update Unsuccessful - Reason: Please check logs - Project:{deployment_detail.release_project_name} Release:{deployment_detail.release_name}', 
+                              f'INFO:root:The failed release update was not curcial. Continuing... Project:{deployment_detail.release_project_name} Release:{deployment_detail.release_name}']
+        
+        # Act
+        with self.assertLogs(level=logging.INFO) as cm:
+            UpdateRelease.handle_failed_update(mock_update_release, deployment_detail)     
+        # Assert
+        self.assertEqual(cm.output, noncrucial_message)
 
     @patch('packages.utils.release_manager.update_release.UpdateRelease', return_value=None)
     def test_update_release_env_notfound_should_fail(self, mock_update_release):
