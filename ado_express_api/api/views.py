c173d96f63dbba3596ceb8854008cdf1673e09d0
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-02-11 21:48:44 -0500

    init simple api

---
+++ ado_express_api/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import ItemSerializer
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+  
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+  
+    return Response(serializer.data)
---
+++ ado_express_api/base/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
