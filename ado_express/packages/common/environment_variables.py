1684cf3f41e30bddc0cbbe46823b245cd09effd1
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-02-16 18:20:41 -0500

    Enable explicit release inclusion & exclusion

--- ado_express/packages/common/environment_variables.py
+++ ado_express/packages/common/environment_variables.py
@@ -1,8 +1,11 @@
+import ast
 import os
 import re
 import sys
 import validators
 from dotenv import load_dotenv
+
+from packages.common.enums.explicit_release_types import ExplicitReleaseTypes
 
 none_types = ["none", "null", "nill", " ", ""]
 
@@ -62,21 +65,31 @@ def get_validated_bool_input(index, key):
 
         if str_input.lower() in ("true", "1", "t"):
             return True
-            
+
+def get_validated_object(index, key):
+    if len(sys.argv) > index and sys.argv[index] is not None:
+        str_input = sys.argv[index].strip()
+    elif os.getenv(key) is not None:
+        str_input = os.getenv(key).strip()
+    else: return None
+
+    return ast.literal_eval(str_input)           
+
 class EnvironmentVariables:
 
     load_dotenv()
 
-    CRUCIAL_RELEASE_DEFINITIONS = get_validated_list_input(1, "CRUCIAL_RELEASE_DEFINITIONS") # cmd arg 1
-    ORGANIZATION_URL = get_validated_string_input(2, "ORGANIZATION_URL", "url") # cmd arg 2
-    PERSONAL_ACCESS_TOKEN = get_validated_string_input(3, "PERSONAL_ACCESS_TOKEN") # cmd arg 3
-    QUERIES = get_validated_list_input(4, "QUERIES") # cmd arg 4
-    RELEASE_NAME_FORMAT = get_validated_string_input(5, "RELEASE_NAME_FORMAT") # cmd arg 5
-    RELEASE_TARGET_ENV = get_validated_string_input(6, "RELEASE_TARGET_ENV") # cmd arg 6
-    SEARCH_ONLY = get_validated_bool_input(7, "SEARCH_ONLY") # cmd arg 7
-    VIA_ENV = get_validated_bool_input(8, "VIA_ENV") # cmd arg 8
-    VIA_ENV_LATEST_RELEASE = get_validated_bool_input(9, "VIA_ENV_LATEST_RELEASE") # cmd arg 9
-    VIA_ENV_SOURCE_NAME = get_validated_string_input(10, "VIA_ENV_SOURCE_NAME") # cmd arg 10
+    EXPLICIT_RELEASE_VALUES = get_validated_object(1, "EXPLICIT_RELEASE_VALUES") # cmd arg 1
+    CRUCIAL_RELEASE_DEFINITIONS = get_validated_list_input(2, "CRUCIAL_RELEASE_DEFINITIONS") # cmd arg 2
+    ORGANIZATION_URL = get_validated_string_input(3, "ORGANIZATION_URL", "url") # cmd arg 3
+    PERSONAL_ACCESS_TOKEN = get_validated_string_input(4, "PERSONAL_ACCESS_TOKEN") # cmd arg 4
+    QUERIES = get_validated_list_input(5, "QUERIES") # cmd arg 5
+    RELEASE_NAME_FORMAT = get_validated_string_input(6, "RELEASE_NAME_FORMAT") # cmd arg 6
+    RELEASE_TARGET_ENV = get_validated_string_input(7, "RELEASE_TARGET_ENV") # cmd arg 7
+    SEARCH_ONLY = get_validated_bool_input(8, "SEARCH_ONLY") # cmd arg 8
+    VIA_ENV = get_validated_bool_input(9, "VIA_ENV") # cmd arg 9
+    VIA_ENV_LATEST_RELEASE = get_validated_bool_input(10, "VIA_ENV_LATEST_RELEASE") # cmd arg 10
+    VIA_ENV_SOURCE_NAME = get_validated_string_input(11, "VIA_ENV_SOURCE_NAME") # cmd arg 11
 
     if not SEARCH_ONLY and VIA_ENV and VIA_ENV_SOURCE_NAME is None:
         raise Exception("To deploy via stage you must provide a VIA_ENV_SOURCE_NAME")
@@ -86,9 +99,7 @@ class EnvironmentVariables:
         and VIA_ENV_LATEST_RELEASE
         and (VIA_ENV_SOURCE_NAME is None or RELEASE_TARGET_ENV is None)
     ):
-        raise Exception(
-            "To search via stage latest release VIA_ENV_SOURCE_NAME & RELEASE_TARGET_ENV must be provided"
-        )
+        raise Exception("To search via stage latest release VIA_ENV_SOURCE_NAME & RELEASE_TARGET_ENV must be provided")
 
     if (
         SEARCH_ONLY
@@ -96,6 +107,9 @@ class EnvironmentVariables:
         and VIA_ENV
         and (VIA_ENV_SOURCE_NAME is None or RELEASE_TARGET_ENV is None)
     ):
-        raise Exception(
-            "To search query via stage, VIA_ENV_SOURCE_NAME & RELEASE_TARGET_ENV must be provided"
-        )
+        raise Exception("To search query via stage, VIA_ENV_SOURCE_NAME & RELEASE_TARGET_ENV must be provided")
+    
+    if EXPLICIT_RELEASE_VALUES:
+        for key in EXPLICIT_RELEASE_VALUES:
+            if key != ExplicitReleaseTypes.EXCLUDE and key != ExplicitReleaseTypes.INCLUDE:
+                    raise Exception("Please select 'include' or 'exclude' as dictionary key for EXPLICIT_RELEASE_VALUES and provide releases in an array format in the dictionary value\n Example: {'include': ['release-1', 'release-2]}")
