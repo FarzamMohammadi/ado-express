fba38e83b02afd0856547048268af0788b57cf2e
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-02-25 20:03:51 -0500

    Update search via-number tests to match endpoint

--- ado_express_api/tests/search_views/via_number_test.py
+++ ado_express_api/tests/search_views/via_number_test.py
@@ -1,9 +1,11 @@
+import datetime
 import json
 import unittest
 from faker import Faker
 from mock import patch
 from django.test.client import RequestFactory
 from django.contrib.auth.models import AnonymousUser
+from base.models.ReleaseDetails import ReleaseDetails
 
 from api.search_views import search_via_release_number
 from base.models.DeploymentDetails import DeploymentDetails
@@ -23,29 +25,28 @@ class SearchViaLatestRelease(unittest.TestCase):
     @patch('ado_express.main.Startup.search_and_log_details_only')
     @patch('ado_express.main.Startup.load_dependencies', return_value=None)
     @patch('ado_express.main.Startup.initialize_logging', return_value=None)
-    def test_api_call(self, initialize_logging_mock, load_dependencies_mock, get_deployment_detail_from_latest_release_mock):
+    def test_api_call(self, initialize_logging_mock, load_dependencies_mock, search_and_log_details_only_mock):
             # Arrange
             release_details = DeploymentDetails(self.fake.name(), self.fake.name(), 123, 132, False)
-            run_configurations = RunConfigurations({},[],self.fake.name(),self.fake.name(),[self.fake.name()],self.fake.name(),self.fake.name(),True,True,True,self.fake.name(), [release_details, release_details, release_details])
+            run_configurations = RunConfigurations({},[],self.fake.name(),self.fake.name(),[self.fake.name()],self.fake.name(),self.fake.name(),True,True,True,self.fake.name(), [release_details, release_details])
             # Conversion needed to enable 
 
-            deployment = Empty()
-            deployment.release_project_name = self.fake.name()
-            deployment.release_name = self.fake.name()
-            deployment.release_number = self.fake.random_int()
-            deployment.release_rollback = self.fake.random_int()
-            deployment.is_crucial = False
+            release = ReleaseDetails(release_details.release_project_name, release_details.release_name, self.fake.name(), self.fake.name(), True, datetime.datetime.now())
 
-            get_deployment_detail_from_latest_release_mock.return_value = deployment
+            returned_releases = [release, release, release]
+            search_and_log_details_only_mock.return_value = returned_releases
 
             request = self.factory.post('/search/via-number', json.dumps(run_configurations.to_dict_with_lowercase_keys()), content_type='application/json')
             request.user = AnonymousUser()
+
+            returned_results = [dict({'release_definition': release_details.release_name, 'release_name': release_details.release_number, 'results': [release.__dict__ for release in returned_releases]}), 
+                                dict({'release_definition': release_details.release_name, 'release_name': release_details.release_number, 'results': [release.__dict__ for release in returned_releases]})]
 
             # Act
             response = search_via_release_number(request)
 
             # Assert
-            self.assertEqual(response.data, {'releases': json.dumps([deployment.__dict__, deployment.__dict__, deployment.__dict__])})
+            self.assertEqual(response.data, {'releases': json.dumps(returned_results, default=str) })
             self.assertEqual(response.status_code, 200)
 
     def test_api_call_with_missing_field(self):
