cc445845d77aa76af543b65df60135edca65f67f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-03-18 16:07:56 -0400

    Create foundational components

--- ado_express_app/src/App.svelte
+++ ado_express_app/src/App.svelte
@@ -1,47 +1,29 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import RunConfigurationForm from './lib/shared/RunConfigurationForm.svelte'
+	import Navbar from './lib/shared/Navbar.svelte'
 </script>
 
-<main>
+
+
+<main class="container mx-auto px-4">
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+    <Navbar />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-pink-500 to-orange-500 text-white">
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div class="text-gray-900">
+        <h1 class="text-6xl font-bold text-center uppercase mb-4 py-10">ADO EXPRESS</h1>
+    <div>
+      
+  </div>
+
+  <div class="py-10">
+    <RunConfigurationForm />
+  </div>
+
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
