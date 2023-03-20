280006cab04d19e93c85aa96e8fda145ee48eacd
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-03-20 19:28:08 -0400

    Convert models to camelCase & convert them to snake_case before calling the python API endpoint

--- ado_express_app/src/lib/core/services/api/ado-express.ts
+++ ado_express_app/src/lib/core/services/api/ado-express.ts
@@ -1,43 +1,58 @@
 import { DEPLOY, SEARCH_VIA_ENVIRONMENT, SEARCH_VIA_LATEST, SEARCH_VIA_NUMBER, SEARCH_VIA_QUERY } from "../../../api";
 import type { IReleaseDetails } from "../../../models/interfaces/irelease-details.interface";
 import type { IRunConfigurations } from "../../../models/interfaces/irun-configurations.interface";
+import { snakeToCamel } from "../../../utils/snake-to-camel";
 import { JSONHttp } from "../https";
 
 
 export class ADOExpressApi {
 
 	public async searchViaEnvironment(runConfigurations: IRunConfigurations) {
-		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_ENVIRONMENT, runConfigurations)
+		const parsedRunConfigurations = this.convertObjectToCamelCase(runConfigurations);
+
+		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_ENVIRONMENT, parsedRunConfigurations)
 		.then(res => {
 			console.log(res)
 		});
 	}
 
 	public async searchViaLatest(runConfigurations: IRunConfigurations) {
-		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_LATEST, runConfigurations)
+		const parsedRunConfigurations = this.convertObjectToCamelCase(runConfigurations);
+
+		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_LATEST, parsedRunConfigurations)
 		.then(res => {
 			console.log(res)
 		});
 	}
 
 	public async searchViaNumber(runConfigurations: IRunConfigurations) {
-		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_NUMBER, runConfigurations)
+		const parsedRunConfigurations = this.convertObjectToCamelCase(runConfigurations);
+
+		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_NUMBER, parsedRunConfigurations)
 		.then(res => {
 			console.log(res)
 		});
 	}
 
 	public async searchViaQuery(runConfigurations: IRunConfigurations) {
-		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_QUERY, runConfigurations)
+		const parsedRunConfigurations = this.convertObjectToCamelCase(runConfigurations);
+
+		return await JSONHttp.post<[IReleaseDetails]>(SEARCH_VIA_QUERY, parsedRunConfigurations)
 		.then(res => {
 			console.log(res)
 		});
 	}
 
 	public async deploy(runConfigurations: IRunConfigurations) {
-		return await JSONHttp.post<[IReleaseDetails]>(DEPLOY, runConfigurations)
+		const parsedRunConfigurations = this.convertObjectToCamelCase(runConfigurations);
+
+		return await JSONHttp.post<[IReleaseDetails]>(DEPLOY, parsedRunConfigurations)
 		.then(res => {
 			console.log(res)
 		});
+	}
+	
+	private convertObjectToCamelCase = (obj: any) => {
+		return Object.fromEntries(Object.entries(obj).map(([key, value]) => [snakeToCamel(key), value]))
 	}
 }
--- ado_express_app/src/lib/core/services/https/http-json.ts
+++ ado_express_app/src/lib/core/services/https/http-json.ts
@@ -1,12 +1,10 @@
-import type { IRunConfigurations } from "../../../models/interfaces/irun-configurations.interface";
-
 export class JSONHttp {
 	public static async get<T>(url: string): Promise<T> {
 		const res = await fetch(url)
 		return res.json() as Promise<T>;
 	}
 
-	public static async post<T>(url: string, obj: IRunConfigurations): Promise<T> {
+	public static async post<T>(url: string, obj: any): Promise<T> {
 		const res = await fetch(url, {
 			method: 'POST',
 			body: JSON.stringify(obj),
