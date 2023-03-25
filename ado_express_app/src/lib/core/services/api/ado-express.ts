d4273933093d4f7d88f0262bb62ceb664a8b016c
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-03-25 12:49:58 -0400

    Add ado-express api call class for easy access

--- ado_express_app/src/lib/core/services/api/ado-express.ts
+++ ado_express_app/src/lib/core/services/api/ado-express.ts
@@ -1,61 +1,85 @@
 import type { RunConfigurations } from '../../../models/classes/run-configurations.model';
+import type { IDeploymentDetails } from '../../../models/interfaces/ideployment-details.interface';
 import type { IReleaseDetails } from '../../../models/interfaces/irelease-details.interface';
 import { JSONHttp } from '../https';
 import { Endpoints } from './endpoints';
 
 export class ADOExpressApi {
-  public async searchViaEnvironment(runConfigurations: RunConfigurations) {
+
+  public async runADOExpress(runConfigurations: RunConfigurations): Promise<[IReleaseDetails] | [IDeploymentDetails]> {
+    if (runConfigurations.searchOnly) {
+      if (runConfigurations.queries) {
+       return await this.searchViaQuery(runConfigurations);
+      }
+      else if (!runConfigurations.viaEnv && !runConfigurations.viaEnvLatestRelease) {
+       return await this.searchViaNumber(runConfigurations);
+      }
+      else if (runConfigurations.viaEnv && !runConfigurations.viaEnvLatestRelease) {
+       return await this.searchViaEnvironment(runConfigurations);
+      }
+      else if (runConfigurations.viaEnv && runConfigurations.viaEnvLatestRelease) {
+       return await this.searchViaLatest(runConfigurations);
+      }
+    }
+    else {
+      // Deploy
+      return await this.deploy(runConfigurations);
+    }
+  }
+
+
+  private async searchViaEnvironment(runConfigurations: RunConfigurations) {
     const parsedRunConfigurations = runConfigurations.toSnakeCase();
 
     return await JSONHttp.post<[IReleaseDetails]>(
       Endpoints.searchViaEnvironment,
       parsedRunConfigurations
     ).then((res) => {
-      console.log(res);
+      return res;
     });
   }
 
-  public async searchViaLatest(runConfigurations: RunConfigurations) {
+  private async searchViaLatest(runConfigurations: RunConfigurations) {
     const parsedRunConfigurations = runConfigurations.toSnakeCase();
 
     return await JSONHttp.post<[IReleaseDetails]>(
       Endpoints.searchViaLatest,
       parsedRunConfigurations
     ).then((res) => {
-      console.log(res);
+      return res;
     });
   }
 
-  public async searchViaNumber(runConfigurations: RunConfigurations) {
+  private async searchViaNumber(runConfigurations: RunConfigurations) {
     const parsedRunConfigurations = runConfigurations.toSnakeCase();
 
     return await JSONHttp.post<[IReleaseDetails]>(
       Endpoints.searchViaNumber,
       parsedRunConfigurations
     ).then((res) => {
-      console.log(res);
+      return res;
     });
   }
 
-  public async searchViaQuery(runConfigurations: RunConfigurations) {
+  private async searchViaQuery(runConfigurations: RunConfigurations) {
     const parsedRunConfigurations = runConfigurations.toSnakeCase();
 
     return await JSONHttp.post<[IReleaseDetails]>(
       Endpoints.searchViaQuery,
       parsedRunConfigurations
     ).then((res) => {
-      console.log(res);
+      return res;
     });
   }
 
-  public async deploy(runConfigurations: RunConfigurations) {
+  private async deploy(runConfigurations: RunConfigurations) {
     const parsedRunConfigurations = runConfigurations.toSnakeCase();
 
-    return await JSONHttp.post<[IReleaseDetails]>(
+    return await JSONHttp.post<[IDeploymentDetails]>(
       Endpoints.deploy,
       parsedRunConfigurations
     ).then((res) => {
-      console.log(res);
+      return res;
     });
   }
 }
