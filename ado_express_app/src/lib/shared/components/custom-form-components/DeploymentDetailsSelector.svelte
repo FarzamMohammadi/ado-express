1b0123f537f5776703596b2b498ff3808b3f850b
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-04-06 17:48:40 -0400

    In APP get parsed DeploymentDetails from selector & show it based on run method (#81)
    * Update browser tab title
    
    * Make deployment details selector into individual comp
    
    * Fix excel file selector to return DeploymentDetails values
    
    * Get parsed DeplyomentDetails values from custom excel pattern selector
    
    * Show deployment details selection based on run method

---
+++ ado_express_app/src/lib/shared/components/custom-form-components/DeploymentDetailsSelector.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import Tooltip from '../utils/Tooltip.svelte';
+  import ExcelFileInput from './ExcelFileInput.svelte';
+  import ExcelPatternSelector from './ExcelPatternSelector.svelte';
+
+  export let deploymentDetails;
+  export let deploymentSelectorHeaders: string[] = [];
+  export let showInput: boolean;
+  export let deploymentDetailsType;
+  export let customDeploymentDetailsSelector;
+</script>
+
+{#if showInput}
+  <div
+    class="min-w-full border-2 border-gray-200 rounded dark:border-gray-700 mt-2 mb-2 p-2 mx-4"
+    id="deploymentDetails"
+  >
+    <div class="flex justify-center ml-3">
+      <label for="deploymentDetails" class="font-bold text-gray-900 mt-2 mr-1"
+        >Deployment Details</label
+      >
+
+      <Tooltip text="Top tooltip" position="right">
+        <i class="mi mi-circle-information text-gray-900"
+          ><span class="sr-only">Circle information</span></i
+        >
+      </Tooltip>
+    </div>
+
+    <div class="pb-2 pt-2 text-gray-900 flex justify-center">
+      <label class="pr-3">
+        <input
+          type="radio"
+          name="deploymentDetailsType"
+          value="file"
+          bind:group={deploymentDetailsType}
+        />
+        Excel File
+      </label>
+
+      <label>
+        <input
+          type="radio"
+          name="deploymentDetailsType"
+          value="custom"
+          bind:group={deploymentDetailsType}
+        />
+        Manual Input
+      </label>
+    </div>
+
+    {#if deploymentDetailsType === 'custom'}
+      <div class="p-2">
+        <ExcelPatternSelector
+          rows={4}
+          headers={deploymentSelectorHeaders}
+          bind:this={customDeploymentDetailsSelector}
+        />
+      </div>
+    {:else if deploymentDetailsType === 'file'}
+      <div class="p-2">
+        <ExcelFileInput bind:deploymentDetails />
+      </div>
+    {/if}
+  </div>
+{/if}
--- ado_express_app/src/lib/shared/components/custom-form-components/ExcelFileInput.svelte
+++ ado_express_app/src/lib/shared/components/custom-form-components/ExcelFileInput.svelte
@@ -1,43 +1,64 @@
 <script lang="ts">
-	import * as XLSX from 'xlsx';
-	import type { DeploymentDetails } from '../../../models/classes/deployment-details.model';
-  
-    let file: File;
-    export let deploymentDetails: DeploymentDetails[] = [];
-  
-    function handleFileUpload(e: Event) {
-      file = (e.target as HTMLInputElement).files[0];
-      const reader = new FileReader();
-  
-      reader.onload = (event) => {
-        const data = new Uint8Array(event.target.result);
-        const workbook = XLSX.read(data, { type: 'array' });
-  
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-  
-        const jsonData = XLSX.utils.sheet_to_json<DeploymentDetails>(worksheet);
-  
-        deploymentDetails = jsonData;
-      };
-  
-      reader.readAsArrayBuffer(file);
-    }
-  </script>
-  
-  <div class="w-full">
-    <input
-      type="file"
-      id="file-upload"
-      on:change={handleFileUpload}
-      class="hidden"
-      accept=".xlsx, .xls"
-    />
-    <label
-      for="file-upload"
-      class="cursor-pointer px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-blue-800"
-    >
-      Upload Excel File
-    </label>
-  </div>
-  
+  import * as XLSX from 'xlsx';
+  import { DeploymentDetails } from '../../../models/classes/deployment-details.model';
+
+  let file: File;
+  export let deploymentDetails: DeploymentDetails[] = [];
+
+  function getBooleanValue(isCrucialUserInput: String) {
+    const trueValues = ['t', 'true', 'y', 'yes', '1']
+
+    return trueValues.includes(isCrucialUserInput.toLowerCase())
+  }
+
+  function handleFileUpload(e: Event) {
+    file = (e.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+
+      const range = XLSX.utils.decode_range(worksheet['!ref']);
+
+      const excelData: string[][] = [];
+
+      for (let rowNum = 1; rowNum <= range.e.r; rowNum++) {
+        const rowData: string[] = [];
+
+        for (let colNum = 0; colNum < 5; colNum++) {
+          const cellAddress = XLSX.utils.encode_cell({ r: rowNum, c: colNum });
+          rowData.push(worksheet[cellAddress]?.v || '');
+        }
+
+        excelData.push(rowData);
+      }
+
+      deploymentDetails = excelData.map(
+        (item: any) =>
+          new DeploymentDetails(item[0], item[1], item[2], item[3], getBooleanValue(item[4]))
+      );
+    };
+
+    reader.readAsArrayBuffer(file);
+  }
+</script>
+
+<div class="w-full mt-1 mb-1">
+  <input
+    type="file"
+    id="file-upload"
+    on:change={handleFileUpload}
+    class="hidden"
+    accept=".xlsx, .xls"
+  />
+  <label
+    for="file-upload"
+    class="cursor-pointer px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-blue-800"
+  >
+    Upload Excel File
+  </label>
+</div>
