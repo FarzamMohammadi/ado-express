27ea752d25f859551811079c00f0e3e75a7f5879
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-04-07 16:55:29 -0400

    Add description under page title

--- ado_express_app/src/App.svelte
+++ ado_express_app/src/App.svelte
@@ -20,11 +20,16 @@
     class="flex flex-col items-center min-w-full min-h-screen justify-center to-gray-600 pb-8"
   >
     <div class="text-center">
-      <h1 class="text-6xl font-bold uppercase py-10 text-gray-900">
+      <h1 class="text-6xl font-bold uppercase pt-10 pb-5 text-gray-900">
         ADO EXPRESS
       </h1>
 
-      <hr class=" border-gray-900 border-t-4 mx-auto mb-8 w-2/12" />
+
+      <p class="text-md text-gray-900 pb-8">
+        A release management tool designed to streamline the Azure DevOps release deployment process.
+      </p>
+
+      <hr class="border-gray-900 border-t-4 mx-auto mb-8 w-2/12" />
     </div>
 
     <div>
