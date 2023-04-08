82eabbdf5aef96eac9afb0b022c5d45e868a7cae
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-04-08 19:54:23 -0400

    Add gh-pages dependencies & configs

--- ado_express_app/vite.config.ts
+++ ado_express_app/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [svelte()],
+  base: isProduction ? '/ado-express/' : '/',
 });
